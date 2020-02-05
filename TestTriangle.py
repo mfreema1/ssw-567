a2a61686b0f3e62967ab1f8cfd39971a08af31e9
mfreema1 <mfreema1@stevens.edu>
2020-02-04 23:21:52 -0500

    Upload new tests

--- TestTriangle.py
+++ TestTriangle.py
@@ -9,24 +9,36 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 
 import unittest
 
-from Triangle import classifyTriangle
-
-# This code implements the unit test functionality
-# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+from Triangle import classify_triangle
 
 class TestTriangles(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
-
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classify_triangle(3, 4, 5), ('Scalene', 'Right'), '3,4,5 is a Right triangle')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        self.assertEqual(classify_triangle(5, 3, 4), ('Scalene', 'Right'), '5,3,4 is a Right triangle')
         
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classify_triangle(2, 2, 2), ('Equilateral', 'Non-Right'), '2,2,2 should be equilateral')
+
+    def testIsoscelesTriangles(self):
+        self.assertEqual(classify_triangle(4, 5, 5), ('Isosceles', 'Non-Right'), '4, 5, 5 should be isosceles')
+
+    def testScaleneTriangles(self):
+        self.assertEqual(classify_triangle(2, 3, 4), ('Scalene', 'Non-Right'), '2, 3, 4 should be isosceles')
+
+    def testNotATriangle(self):
+        with self.assertRaises(ValueError, msg='1, 1, 100 is not a valid triangle'):
+            classify_triangle(1,1, 100)
+
+    def testNegativeSideLengths(self):
+        with self.assertRaises(ValueError, msg='-1, 1, 1 contains a negative side distance'):
+            classify_triangle(-1, 1, 1)
+
+    def testWrongArgumentTypes(self):
+        with self.assertRaises(ValueError, msg='foo, bar, and baz are not integers'):
+            classify_triangle('foo', 'bar', 'baz')
 
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
-
