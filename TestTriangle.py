82b1312dea967192eae0f4574ee880e3c3dd2895
mfreema1 <mfreema1@stevens.edu>
2020-02-25 16:24:57 -0500

    Lint code and tests

--- TestTriangle.py
+++ TestTriangle.py
@@ -12,30 +12,68 @@ import unittest
 from Triangle import classify_triangle
 
 class TestTriangles(unittest.TestCase):
-    def testRightTriangleA(self): 
-        self.assertEqual(classify_triangle(3, 4, 5), ('Scalene', 'Right'), '3,4,5 is a Right triangle')
+    """
+    Verify that the Triangle class performs as expected.
+    """
 
-    def testRightTriangleB(self): 
-        self.assertEqual(classify_triangle(5, 3, 4), ('Scalene', 'Right'), '5,3,4 is a Right triangle')
-        
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classify_triangle(2, 2, 2), ('Equilateral', 'Non-Right'), '2,2,2 should be equilateral')
+    def test_right_triangle_a(self):
+        """
+        Test ordering of a right triangle
+        """
+        self.assertEqual(classify_triangle(3, 4, 5),
+                         ('Scalene', 'Right'),
+                         '3,4,5 is a Right triangle')
 
-    def testIsoscelesTriangles(self):
-        self.assertEqual(classify_triangle(4, 5, 5), ('Isosceles', 'Non-Right'), '4, 5, 5 should be isosceles')
+    def test_right_triangle_b(self):
+        """
+        Test ordering of a right triangle with swapped ordering
+        """
+        self.assertEqual(classify_triangle(5, 3, 4),
+                         ('Scalene', 'Right'),
+                         '5,3,4 is a Right triangle')
 
-    def testScaleneTriangles(self):
-        self.assertEqual(classify_triangle(2, 3, 4), ('Scalene', 'Non-Right'), '2, 3, 4 should be isosceles')
+    def test_equilateral_triangles(self):
+        """
+        Test an equilateral triangle
+        """
+        self.assertEqual(classify_triangle(2, 2, 2),
+                         ('Equilateral', 'Non-Right'),
+                         '2,2,2 should be equilateral')
 
-    def testNotATriangle(self):
+    def test_isosceles_triangles(self):
+        """
+        Test an isosceles triangle
+        """
+        self.assertEqual(classify_triangle(4, 5, 5),
+                         ('Isosceles', 'Non-Right'),
+                         '4, 5, 5 should be isosceles')
+
+    def test_scalene_triangles(self):
+        """
+        Test a scalene triangle
+        """
+        self.assertEqual(classify_triangle(2, 3, 4),
+                         ('Scalene', 'Non-Right'),
+                         '2, 3, 4 should be isosceles')
+
+    def test_not_a_triangle(self):
+        """
+        Test a triangle that is not possible to close
+        """
         with self.assertRaises(ValueError, msg='1, 1, 100 is not a valid triangle'):
-            classify_triangle(1,1, 100)
+            classify_triangle(1, 1, 100)
 
-    def testNegativeSideLengths(self):
+    def test_negative_side_lengths(self):
+        """
+        Test a triangle with a negative side length
+        """
         with self.assertRaises(ValueError, msg='-1, 1, 1 contains a negative side distance'):
             classify_triangle(-1, 1, 1)
 
-    def testWrongArgumentTypes(self):
+    def test_wrong_argument_types(self):
+        """
+        Test a triangle with incorrect parameter type
+        """
         with self.assertRaises(ValueError, msg='foo, bar, and baz are not integers'):
             classify_triangle('foo', 'bar', 'baz')
 
--- Triangle.py
+++ Triangle.py
@@ -1,38 +1,43 @@
+"""
+A collection of functions to help with classifying triangles
+"""
+
 from collections import Counter
 
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 14 13:44:00 2016
-Updated Jan 21, 2018
-
-The primary goal of this file is to demonstrate a simple python program to classify triangles
-
-@author: jrr
-@author: rk
-"""
-
-def is_right_triangle(a, b, c):
-    sorted_squares = list(map(lambda num: pow(num, 2), sorted([a, b, c])))
+def is_right_triangle(first, second, third):
+    """
+    Determine if a set of sides constitutes a right triangle
+    """
+    sorted_squares = list(map(lambda num: pow(num, 2), sorted([first, second, third])))
     return sum(sorted_squares[:2]) == sorted_squares[2]
 
-def is_possible_triangle(a, b, c):
-    sorted_sides = sorted([a, b, c])
+def is_possible_triangle(first, second, third):
+    """
+    Determine if a set of sides could constitute a triangle
+    """
+    sorted_sides = sorted([first, second, third])
     return sum(sorted_sides[:2]) > sorted_sides[2]
 
 def num_distinct(nums):
+    """
+    Determine the number of distinct elements in a list
+    """
     counter = Counter()
     for num in nums:
         counter[num] += 1
     return len(counter.keys())
 
-def classify_triangle(a, b, c):
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
+def classify_triangle(first, second, third):
+    """
+    Main method of module.  Export information about a set of sides
+    """
+    if not(isinstance(first, int) and isinstance(second, int) and isinstance(third, int)):
         raise ValueError("Arguments must be integers")
 
-    if not(a > 0 and b > 0 and c > 0):
+    if not(first > 0 and second > 0 and third > 0):
         raise ValueError("Side lengths cannot be negative")
 
-    if not is_possible_triangle(a, b, c):
+    if not is_possible_triangle(first, second, third):
         raise ValueError("Not a triangle")
 
     types = {
@@ -41,18 +46,9 @@ def classify_triangle(a, b, c):
         3: 'Scalene'
     }
 
-    is_right = is_right_triangle(a, b, c)
-    distinct_sides = num_distinct([a, b, c])
+    is_right = is_right_triangle(first, second, third)
+    distinct_sides = num_distinct([first, second, third])
 
     right_string = 'Right' if is_right else 'Non-Right'
 
     return (types[distinct_sides], right_string)
-
-    """
-    return:
-        If all three sides are equal, return 'Equilateral'
-        If exactly one pair of sides are equal, return 'Isoceles'
-        If no pair of  sides are equal, return 'Scalene'
-        If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
-    """
